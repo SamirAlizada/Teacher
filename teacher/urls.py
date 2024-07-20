de711eed76892fd1f5a71d0cdd215b5693dce047
SamirAlizada <samir.elizade5555@gmail.com>
2024-07-20 17:26:29 +0400

    Update URL patterns for student management and payment handling
    - Changed `add_student` URL path to `student/add/`.
    - Added `update_student_pay` view with path `student/update/pay/<int:pk>/`.
    - Added `pay_day` view with path `pay_day/`.
    - Ensured consistency in URL naming and paths for student-related operations.

--- teacher/urls.py
+++ teacher/urls.py
@@ -5,15 +5,18 @@ urlpatterns = [
     path('', group_list, name='group_list'),
     path('group/add/', add_group, name='add_group'),
     path('group/<int:group_id>/', group_detail, name='group_detail'),
-    path('add-student/', add_student, name='add_student'),
+    path('student/add/', add_student, name='add_student'),
 
     #Update
     path('update-group/<int:pk>/', update_group, name='update_group'),
     path('student/update/<int:pk>/<int:group_id>/', update_student, name='update_student'),
+    path('student/update/pay/<int:pk>/', update_student_pay, name='update_student_pay'),
 
     # Delete
     path('delete-group/<int:pk>/', delete_group, name='delete_group'),
     path('delete-student/<int:pk>/', delete_student, name='delete_student'),
+
+    path('pay_day/', pay_day, name='pay_day'),
 
     # Account
     path('login/', user_login, name='login'),
